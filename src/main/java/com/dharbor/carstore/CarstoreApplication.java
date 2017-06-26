6ce91ff606f6c43608acedc6cbda14e49392600d
Martin Torrico Vargas <alantorrico@gmail.com>
2017-06-26 05:34:54 -0400

    Car store structure

---
+++ src/main/java/com/dharbor/carstore/CarstoreApplication.java
@@ -0,0 +1,12 @@
+package com.dharbor.carstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarstoreApplication.class, args);
+	}
+}
